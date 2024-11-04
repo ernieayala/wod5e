726ce586d786dbc36ff8916c7aa679b2b7381d43
Ernesto Ayala <teafortwo.ea@gmail.com>
2024-11-03 20:37:41 -0600

    make ui less shitty

--- system/scripts/helpers.js
+++ system/scripts/helpers.js
@@ -152,4 +152,8 @@ export const loadHelpers = async function () {
     }
     return edges[key]
   })
+
+  Handlebars.registerHelper('json', function(context) {
+    return JSON.stringify(context, null, 2); // Pretty-prints with 2-space indentation
+  })
 }
